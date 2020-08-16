f3a692cdd108f0d1266a5ddecdbf636c70d67455
Trond Bergquist <trond_bergquist@hotmail.com>
2020-08-16 10:42:23 +0200

    Use @tberg/theme in my-app

--- src/my-app/src/index.js
+++ src/my-app/src/index.js
@@ -2,9 +2,22 @@
 
 import * as React from 'react';
 import { render } from 'react-dom';
+import theme from '@tbergq/theme';
 
 function App() {
-  return <div>hey you</div>;
+  return (
+    <div>
+      hey you
+      <div>
+        This is your theme <br />
+        <span
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(theme, null, 2).replace(/\n/g, '<br />'),
+          }}
+        />
+      </div>
+    </div>
+  );
 }
 
 const root = document.querySelector('#root');
