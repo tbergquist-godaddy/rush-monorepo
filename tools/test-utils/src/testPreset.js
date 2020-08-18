888e7b83c6cfd7a3f857adfa22b670c12f2aa86d
Trond Bergquist <trond_bergquist@hotmail.com>
2020-08-18 21:33:07 +0200

    Remove isCI usage in test preset
    - Log some test status during test run

--- tools/test-utils/src/testPreset.js
+++ tools/test-utils/src/testPreset.js
@@ -1,17 +1,14 @@
 const path = require('path');
-const isCi = require('is-ci');
 
 const config = {};
 
-if (isCi) {
-  config.reporters = [
-    [
-      (path.join(__dirname, 'JestReporter.js') /*: string */),
-      {
-        outputFilePath: (path.join(__dirname, 'test-results.json') /*: string */),
-      },
-    ],
-  ];
-}
+config.reporters = [
+  [
+    (path.join(__dirname, 'JestReporter.js') /*: string */),
+    {
+      outputFilePath: (path.join(__dirname, 'test-results.json') /*: string */),
+    },
+  ],
+];
 
 module.exports = config;
