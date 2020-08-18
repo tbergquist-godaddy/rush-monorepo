007c00173c40389c47a5129e05ee3c30384c0f05
Trond Bergquist <trond_bergquist@hotmail.com>
2020-08-18 21:33:07 +0200

    Remove isCI usage in test preset
    - Log some test status during test run

--- tools/test-utils/src/JestReporter.js
+++ tools/test-utils/src/JestReporter.js
@@ -14,6 +14,20 @@ class MyCustomReporter {
     process.stdout.write(`${message}\n`);
   }
 
+  onTestFileResult(
+    test /*: Object  */,
+    testResult /*: Object  */,
+    aggregatedResult /*: Object  */,
+  ) {
+    const status = testResult.numFailingTests === 0 ? 'Passed' : 'Failed';
+    const runtime = Number(testResult.perfStats.runtime / 1000).toPrecision(2);
+    this.log(`${status}: ${testResult.testFilePath}  - ${runtime}s`);
+  }
+
+  onTestFileStart(test/*: Object  */) {
+    this.log(`Runs: ${test.path}`);
+  }
+
   onRunComplete(contexts /*: Object */, results /*: Object */) {
     this.log(`
     passed: ${results.numPassedTests}
