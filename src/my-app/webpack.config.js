c857c0daf47d29cd70206d3f64c20170db4d6bca
Trond Bergquist <trond_bergquist@hotmail.com>
2020-08-22 23:19:59 +0200

    my-app: Webpack improvements

--- src/my-app/webpack.config.js
+++ src/my-app/webpack.config.js
@@ -1,28 +1,35 @@
 // @flow
 
 const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: (path.resolve(__dirname, 'dist') /*: string */),
   },
   plugins: [
-    (new CopyPlugin({
-      patterns: [
-        {
-          from: path.join(__dirname, 'src', 'index.html'),
-          to: path.join(__dirname, 'dist', 'index.html'),
-        },
-      ],
-    }) /*: CopyPlugin */),
+    (new CleanWebpackPlugin() /*: CleanWebpackPlugin */),
+    (new HtmlWebpackPlugin({
+      title: 'My app',
+      hash: true,
+      scriptLoading: 'defer',
+      template: 'src/index.html',
+    }) /*: HtmlWebpackPlugin */),
   ],
   devServer: {
     contentBase: (path.join(__dirname, 'dist') /*: string */),
     compress: true,
     port: 9000,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
   },
   module: {
     rules: [
