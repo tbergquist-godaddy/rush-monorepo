abf97818980d209c53e9895bac4c647abcd45c26
Trond Bergquist <trond_bergquist@hotmail.com>
2020-09-11 15:39:36 +0200

    Setup fbt for team assistant

--- src/team-assistant/src/home/home.js
+++ src/team-assistant/src/home/home.js
@@ -2,11 +2,14 @@
 
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import fbt from 'fbt';
 
 export default function Home(): React.Node {
   return (
     <div>
-      <h1>Home TODO</h1>
+      <h1>
+        <fbt desc="Todo text">Home TODO</fbt>
+      </h1>
       <Link to="/login">Go home</Link>
     </div>
   );
