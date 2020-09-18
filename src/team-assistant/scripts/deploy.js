b6bbf0aadf80f200cf8ac22e4acf6abfe9996747
Trond Bergquist <trond_bergquist@hotmail.com>
2020-09-18 17:11:27 +0200

    Copy files to deploy with node script

--- src/team-assistant/scripts/deploy.js
+++ src/team-assistant/scripts/deploy.js
@@ -10,10 +10,11 @@ if (ZEIT_TOKEN == null) {
   throw new Error('ZEIT_TOKEN missing.');
 }
 const execSync = (command) => {
-  exec(command, { stdio: 'inherit' });
+  exec(command, { stdio: ['inherit', 'inherit', process.stdout] });
 };
 
 const deployPath = path.join(__dirname, '..', 'deploy', 'public');
+const distPath = path.join(__dirname, '..', 'dist');
 
 if (fs.existsSync(deployPath)) {
   // $FlowExpectedError[extra-arg] This does allow a second arg
@@ -22,5 +23,10 @@ if (fs.existsSync(deployPath)) {
 
 fs.mkdirSync(deployPath, { recursive: true });
 
-execSync('cp -R dist/ deploy/public/');
+const distFiles = fs.readdirSync(distPath);
+
+for (const file of distFiles) {
+  fs.copyFileSync(path.join(distPath, file), path.join(deployPath, file));
+}
+
 execSync(`vercel deploy/ --confirm --prod --token=${ZEIT_TOKEN}`);
